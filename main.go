04be64b0f75a82bfd0e3732fc727f0a3e1df2658
James Dunne <james.jdunne@gmail.com>
2018-02-27 16:44:38 -0600

    Attempts to decode firmware sysex data.

--- main.go
+++ main.go
@@ -9,75 +9,105 @@ import (
 )
 
 func main() {
-	fname := `MoP - Rhythm L`
 	extIn := `.syx`
 	extBin := `.bin`
 	//extRaw := `.raw`
 
-	f, err := os.Open(fname + extIn)
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
+	//fmt.Println(byte(0x7A) ^ byte(0x7F))
 
-	rawBytes := make([]byte, 0, 10240)
-	ob := make([]byte, 1)
-	msg := make([]byte, 0, 4+0xA0+1)
+	//fnames := []string{`MoP - Rhythm L`, `MoP - Rhythm R`, `Puppets - Rhythm L_XL`, `Puppets - Rhythm R_XL`}
+	fnames := []string{`AxeFx2_Quantum_9p04`}
+	for _, fname := range fnames {
 
-	hdr := make([]byte, 4)
-
-	msgs := 0
-	// Read SysEx blocks:
-reading:
-	for {
-		// read SysEx header F0 00 01 74
-		f.Read(hdr)
-		if hdr[0] != 0xF0 || hdr[1] != 0x00 || hdr[2] != 0x01 || hdr[3] != 0x74 {
-			break
+		f, err := os.Open(fname + extIn)
+		if err != nil {
+			panic(err)
 		}
+		defer f.Close()
 
-		// Read until 0xF7:
-		msg = msg[0:0]
-		ob[0] = byte(0xF0)
-		for i := 0; ob[0] != 0xF7; i++ {
-			n, err := f.Read(ob)
-			if n <= 0 || err != nil {
-				break reading
-			}
-			if ob[0] == 0xF7 {
+		rawBytes := make([]byte, 0, 10240)
+		ob := make([]byte, 1)
+		msg := make([]byte, 0, 4+0xA0+1)
+
+		hdr := make([]byte, 4)
+
+		msgs := 0
+		// Read SysEx blocks:
+	reading:
+		for {
+			// read SysEx header F0 00 01 74
+			f.Read(hdr)
+			if hdr[0] != 0xF0 || hdr[1] != 0x00 || hdr[2] != 0x01 || hdr[3] != 0x74 {
 				break
 			}
 
-			msg = append(msg, ob[0])
-		}
-
-		// 03 7B 20 00 is a data packet for IR:
-		if msg[0] == 0x03 && msg[1] == 0x7B {
-			msgs++
-			data7 := msg[4 : len(msg)-1]
-			data := make([]byte, len(data7)*4/5)
-			j := 0
-			for i := 0; i < len(data7); i += 5 {
-				if data7[i+4]&^0x0F != 0 {
-					panic("Unexpected extra bits in final byte of 5-byte string")
+			// TODO: optimize with readCount = (msg[4] * 5)
+			// Read until 0xF7:
+			msg = msg[0:0]
+			ob[0] = byte(0xF0)
+			for i := 0; ob[0] != 0xF7; i++ {
+				n, err := f.Read(ob)
+				if n <= 0 || err != nil {
+					break reading
 				}
-				b := sysexToRaw(data7[i : i+5])
-				binary.LittleEndian.PutUint32(data[j:j+4], b)
-				j += 4
+				if ob[0] == 0xF7 {
+					break
+				}
+
+				msg = append(msg, ob[0])
 			}
-			rawBytes = append(rawBytes, data...)
+
+			// 03 7B 20 00 is a data packet for IR:
+			if (msg[0] == 0x03 || msg[0] == 0x06) && (msg[1] == 0x7B) {
+				msgs++
+				data7 := msg[4 : len(msg)-1]
+				data := make([]byte, len(data7)*4/5)
+				j := 0
+				for i := 0; i < len(data7); i += 5 {
+					if data7[i+4]&^0x0F != 0 {
+						panic("Unexpected extra bits in final byte of 5-byte string")
+					}
+					b := sysexToRaw(data7[i : i+5])
+					binary.LittleEndian.PutUint32(data[j:j+4], b)
+					j += 4
+				}
+				rawBytes = append(rawBytes, data...)
+			}
+
+			// 03 7E 20 00 is a data packet for firmware:
+			if (msg[0] == 0x03 || msg[0] == 0x06) && (msg[1] == 0x7E) {
+				msgs++
+				data7 := msg[4 : len(msg)-1]
+				data78 := make([]byte, 8)
+				for i := 0; i < len(data7); i += 5 {
+					var u32 uint64
+					last4bits := (int8(data7[i+4]<<1) >> 1)
+					if last4bits < 0 {
+						last4bits += 16
+					}
+					u32 = uint64(data7[i+0]&0x7F)<<0 |
+						uint64(data7[i+1]&0x7F)<<7 |
+						uint64(data7[i+2]&0x7F)<<14 |
+						uint64(data7[i+3]&0x7F)<<21 |
+						uint64(last4bits)<<28
+
+					binary.LittleEndian.PutUint64(data78, u32)
+					rawBytes = append(rawBytes, data78[0:4]...)
+				}
+			}
+
+			// Final message:
+			if (msg[0] == 0x03 || msg[0] == 0x06) && (msg[1] == 0x7C || msg[1] == 0x7F) {
+				b := sysexToRaw(msg[2 : 2+5])
+				data := make([]byte, 4)
+				binary.LittleEndian.PutUint32(data[0:4], b)
+				fmt.Println(data)
+			}
 		}
 
-		// Final message:
-		if msg[0] == 0x03 && msg[1] == 0x7C {
-			b := sysexToRaw(msg[2 : 2+5])
-			data := make([]byte, 4)
-			binary.LittleEndian.PutUint32(data[0:4], b)
-			fmt.Println(data)
-		}
+		fmt.Println(fname + extBin)
+		ioutil.WriteFile(fname+extBin, rawBytes, 0644)
 	}
-
-	ioutil.WriteFile(fname+extBin, rawBytes, 0644)
 
 	// fmt.Printf("%v\n", msgs)
 	// fmt.Printf("%v\n", len(rawBytes))
